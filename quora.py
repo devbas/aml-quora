4f17e79b52f4cf81998232eb1b1567c9854b2cc0
Bastian Geneugelijk <bastiangeneugelijk@me.com>
2017-11-26 17:54:30 +0100

    - NLTK Corpus added - Split text - Stopword removal

--- quora.py
+++ quora.py
@@ -1,4 +1,10 @@
-''' Prerequisites: The project uses Python3 among the following libraries: 
+''' Prerequisites: 
+
+- The project uses Python3
+- Make sure you have the nltk corpus installed:
+	* import nltk 
+	* nltk.download()
+	* Download all collections 
 
 '''
 
@@ -8,12 +14,45 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import os
-from numpy import genfromtxt
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
 
 # 2. User setting 1: Import data
 # Important: data needs to be stored in directory 'data' in parent folder of current working directory
 
 path = os.getcwd()
 os.chdir(path)
-raw_df = pd.read_csv("data/test_data.csv")
-print('number of observations: {:,}'.format(raw_df.shape[0]))
+train_df = pd.read_csv("data/train_data.csv", nrows=20, delimiter=',')
+#train_df.head()
+print('number of observations: {:,}'.format(train_df.shape[0]))
+
+
+# 3. Split text
+def word_tokens(row): 
+
+	output = row;
+	output['question1'] = word_tokenize(str(row['question1']).lower())
+	output['question2'] = word_tokenize(str(row['question2']).lower())
+
+	return output
+
+
+train_tokenized = train_df.apply(word_tokens, axis=1, raw=True)
+
+
+# 4. Stopword removal
+stop_words = set(stopwords.words("english"))
+
+def stopword_removal(row): 
+
+	output = row;
+	output['question1'] = [w for w in row['question1'] if not w in stop_words]
+	output['question2'] = [w for w in row['question2'] if not w in stop_words]
+
+	return output
+
+
+train_tokenized = train_tokenized.apply(stopword_removal, axis=1, raw=True)
+
+print('train tokenized: ', train_tokenized);
