bedec8af3d91cdcade98c0982fed1ff641e4e17a
Jop <jopbrandenburg@gmail.com>
2017-11-28 12:13:20 +0100

    Keras incl test data prediction

--- quora.py
+++ quora.py
@@ -30,6 +30,7 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',level=log
 path = os.getcwd()
 os.chdir(path)
 train_df = pd.read_csv("data/train_data.csv", nrows=500000, delimiter=',')
+test_df = pd.read_csv("data/test_data.csv", nrows=100000, delimiter=',')
 train_labels = pd.read_csv("data/train_labels.csv", nrows=500000, delimiter=',')
 train_labels = train_labels['is_duplicate']
 #train_df.head()
@@ -44,6 +45,7 @@ def word_tokens(row):
   return output
 
 train_tokenized = train_df.apply(word_tokens, axis=1, raw=True)
+test_tokenized = test_df.apply(word_tokens, axis=1, raw=True)
 
 
 # 4. Stopword removal
@@ -73,9 +75,13 @@ def stemming_row(row):
 
 train_tokenized = train_tokenized.apply(stemming_row, axis=1, raw=True)
 
+test_tokenized = test_tokenized.apply(stemming_row, axis=1, raw=True)
 
 # 6. Set vocabulary/Dictionary 
 sentences = [train_tokenized['question1'], train_tokenized['question2']];
+test_sentences = [test_tokenized['question1'], test_tokenized['question2']];
+
+
 
 num_features = 300    # Word vector dimensionality                      
 min_word_count = 40   # Minimum word count                        
@@ -85,11 +91,15 @@ downsampling = 1e-3   # Downsample setting for frequent words
 
 print('sentences: ', sentences);
 
-model = word2vec.Word2Vec(sentences, size=300, window = context, min_count=2, workers=num_workers, sample=downsampling)
+train_model = word2vec.Word2Vec(sentences, size=300, window = context, min_count=2, workers=num_workers, sample=downsampling)
+test_model = word2vec.Word2Vec(test_sentences, size=300, window = context, min_count=2, workers=num_workers, sample=downsampling)
 
-model_name = "model"
-model.save(model_name)
-wordModel = word2vec.Word2Vec.load(model_name)
+
+
+train_model.save("train_model")
+test_model.save("test_model")
+trainWordModel = word2vec.Word2Vec.load(train_model)
+testWordModel = word2vec.Word2Vec.load(test_model)
 
 #print('similarity-test: ',wordModel.wv.most_similar(positive=['muslim'], negative=['man']))
 
@@ -97,7 +107,7 @@ wordModel = word2vec.Word2Vec.load(model_name)
 
 
 # 8. RNN (LSTM)
-def makeTrainModel(x_train, y_train):
+def trainNeuralNet(x_train, y_train):
   model = Sequential()
   model.add(Dense(64, input_dim=len(wordModel), activation='relu'))
   model.add(Dense(16, activation='relu'))
@@ -111,5 +121,21 @@ def makeTrainModel(x_train, y_train):
 
   model.fit(x_train, y_train, epochs=10, batch_size=32)
 
+def testNeuralNet(x_test):
+  model = Sequential()
+  model.add(Dense(64, input_dim=len(wordModel), activation='relu'))
+  model.add(Dense(16, activation='relu'))
+  model.add(Dense(16, activation='relu'))
+  model.add(Dense(16, activation='relu'))
+  model.add(Dense(1, activation='sigmoid'))
 
-makeTrainModel(wordModel, train_labels)
+  model.compile(optimizer='rmsprop',
+                loss='binary_crossentropy',
+                metrics=['accuracy'])
+
+  model.predict(x_test, batch_size=32)
+
+
+
+trainNeuralNet(trainWordModel, train_labels)
+testNeuralNet(testWordModel)
