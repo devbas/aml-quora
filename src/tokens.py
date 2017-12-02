2e9938e75e4769c1f488de40ecba121dc590b173
Bastian Geneugelijk <bastiangeneugelijk@me.com>
2017-12-02 12:24:19 +0100

    Code cleaning

--- src/tokens.py
+++
@@ -1,9 +0,0 @@
-from nltk.tokenize import word_tokenize
-
-def word_tokens(row): 
-   
-  output = row;
-  output['question1'] = word_tokenize(str(row['question1']).lower())
-  output['question2'] = word_tokenize(str(row['question2']).lower())
-
-  return output
