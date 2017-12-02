929ad2742d7cba2687d0241bd55c377931af7925
Bastian Geneugelijk <bastiangeneugelijk@me.com>
2017-12-02 16:33:13 +0100

    TF-IDF initiated

--- model.py
+++ model.py
@@ -2,22 +2,25 @@
 
 import numpy as np
 import pandas as pd
+import sys
 import os
 import gensim
 from tqdm import tqdm
 from sklearn.feature_extraction.text import TfidfVectorizer
+import logging
 
 from utils.stemming import stemming_row
 from utils.tokens import word_tokens
 from utils.dictionary import create_dict
 from utils.vec_features import vec_features
 
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',level=logging.INFO)
 
 # 2. User setting 1: Import data
 # Important: data needs to be stored in directory 'data' in parent folder of current working directory
 path = os.getcwd()
 os.chdir(path)
-train_df = pd.read_csv("data/train_data.csv", delimiter=',')
+train_df = pd.read_csv("data/train_data.csv", nrows=10000, delimiter=',')
 
 questions = list(train_df['question1']) + list(train_df['question2'])
 
@@ -51,9 +54,12 @@ model.save('output/word2vec.mdl')
 model.wv.save_word2vec_format('output/word2vec.bin', binary=True)
 
 
+# -------------------------------------------- SECTION 2 
+# As you see, we start over with `train_df` in TFIDF
+
 # 7. TF-IDF 
 tfidf = TfidfVectorizer(lowercase=False)
-tfidf.fit_transform(questions)
+tfidf.fit_transform(list(train_df['question1']) + list(train_df['question2']))
 
 word2tfidf = dict(zip(tfidf.get_feature_names(), tfidf.idf_))
 
