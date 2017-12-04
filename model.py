d8331d07fd23b533bbbe0abeeaca95a331bbc9b1
Bastian Geneugelijk <bastiangeneugelijk@me.com>
2017-12-04 13:53:40 +0100

    Improvements

--- model.py
+++ model.py
@@ -6,7 +6,6 @@ import sys
 import os
 import gensim
 from tqdm import tqdm
-from sklearn.feature_extraction.text import TfidfVectorizer
 from keras.preprocessing import sequence
 from keras.models import Sequential
 from keras.layers import Dense, Activation
@@ -15,9 +14,9 @@ import logging
 from utils.stemming import stemming_row
 from utils.tokens import word_tokens
 from utils.dictionary import create_dict
-from utils.vec_features import vec_features
-from utils.euclidean_distance import euclidean_distance
+from utils.tfidf import tfidf
 from utils.longest import longest_question
+from utils.distance import distance
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',level=logging.INFO)
 
@@ -26,39 +25,35 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',level=log
 path = os.getcwd()
 os.chdir(path)
 train_df = pd.read_csv("data/train_data.csv", delimiter=',')
+test_df = pd.read_csv("data/test_data.csv", delimiter=',')
 train_duplicate = pd.read_csv("data/train_labels.csv", delimiter=',')
 
-questions = list(train_df['question1'].values.astype('U')) + list(train_df['question2'].values.astype('U'))
+#questions = list(train_df['question1'].values.astype('U')) + list(train_df['question2'].values.astype('U'))
 
 
 # -------------------------------------------- SECTION 2 
 # As you see, we start over with `train_df` in TFIDF
 
 # 7. TF-IDF 
-tfidf = TfidfVectorizer(lowercase=False)
-tfidf.fit_transform(questions)
-
-word2tfidf = dict(zip(tfidf.get_feature_names(), tfidf.idf_)) # To research
-
-print('Features to vec')
-train_df['q1_feats'] = vec_features(train_df['question1'], word2tfidf)
-train_df['q2_feats'] = vec_features(train_df['question2'], word2tfidf)
+train_df['q1_feats'] = tfidf(train_df['question1'])
+train_df['q2_feats'] = tfidf(train_df['question2'])
+test_df['q1_feats'] = tfidf(test_df['question1'])
+test_df['q2_feats'] = tfidf(test_df['question2'])
 
 
 # 8. Train set
-longest = longest_question(train_df['q1_feats'], train_df['q2_feats'])
+train_longest = longest_question(train_df['q1_feats'], train_df['q2_feats'])
+test_longest = longest_question(test_df['q1_feats'], test_df['q2_feats'])
 
-questions1 = sequence.pad_sequences(train_df['q1_feats'], longest)
-questions2 = sequence.pad_sequences(train_df['q2_feats'], longest)
+train_questions1 = sequence.pad_sequences(train_df['q1_feats'], train_longest)
+train_questions2 = sequence.pad_sequences(train_df['q2_feats'], train_longest)
 
+test_questions1 = sequence.pad_sequences(test_df['q1_feats'], test_longest)
+test_questions2 = sequence.pad_sequences(test_df['q2_feats'], test_longest)
 
-distances= np.zeros(len(questions1))
-counter = 0
+train_distances = distance(train_questions1, train_questions2)
+test_distances = distance(test_questions1, test_questions2)
 
-for q1, q2 in zip(questions1, questions2): 
-	distance = euclidean_distance(q2, q1)
-	distances[counter] = distance
-	counter += 1
 
 model = Sequential()
 model.add(Dense(64, input_dim=1, activation='relu'))
@@ -69,9 +64,20 @@ model.add(Dense(1, activation='sigmoid'))
 
 model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
 
-print('distances: ', distances)
+#print('distances: ', distances)
 
-model.fit(distances, train_duplicate['is_duplicate'], epochs=10, batch_size=32)
+model.fit(train_distances, train_duplicate['is_duplicate'], epochs=10, batch_size=32)
+predictions = model.predict(test_distances, verbose=1)
+
+rounded = [int(round(x[0])) for x in predictions]
+
+submission_df = pd.DataFrame(index=test_df.test_id, columns=['is_duplicate'], dtype=np.uint)
+submission_df.index.name = 'test_id'
+submission_df.is_duplicate = rounded
+
+submission_df.to_csv('output/submission.csv')
+
+print('predications: ', predictions)
 
 #for counter, distance in enumerate(distances): 
 	#print(counter, '  distance:', distance, ' is duplicate: ', train_duplicate['is_duplicate'][counter])
