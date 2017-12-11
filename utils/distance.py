c9517cdeff1369ace314a60b77b8de871d4e8911
Bastian Geneugelijk <bastiangeneugelijk@me.com>
2017-12-11 15:38:20 +0100

    Distance util changed

--- utils/distance.py
+++ utils/distance.py
@@ -2,16 +2,25 @@ import numpy as np
 from utils.euclidean_distance import euclidean_distance
 from scipy import spatial
 
-def distance(questions1, questions2): 
+def distance(questions1, questions2, method): 
 
 	# We suppose both variables have the same len()
 	distances = np.zeros(len(questions1))
 
 	counter = 0
 	for q1, q2 in zip(questions1, questions2): 
-		distance = 1 - spatial.distance.cosine(q2, q1)
-		#distance = euclidean_distance(q2, q1)
+		
+
+		distance = euclidean_distance(q2, q1) if method == 'euclidean' else 1 - spatial.distance.cosine(q2, q1)
 		distances[counter] = distance
+		#distances[counter] = np.zeros(2)
+		#distances[counter][0] = 1 - spatial.distance.cosine(q2, q1) # Cosine Similarity
+		#distances[counter][1] = euclidean_distance(q2, q1) # Euclidean distance
 		counter += 1
 
 	return distances
+
+
+# TFIDF Vector question 1
+# TFIDF Vector question 2
+# 
